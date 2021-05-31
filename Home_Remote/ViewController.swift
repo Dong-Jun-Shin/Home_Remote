df81f2f8b7ff1220537d171954812b0e6758f247
Dong-Jun-Shin <tlsehdwns239@gmail.com>
2021-05-31 12:45:16 +0900

    Initial Commit

---
+++ Home_Remote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home_Remote
+//
+//  Created by 임시 사용자 (DJ) on 2021/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
